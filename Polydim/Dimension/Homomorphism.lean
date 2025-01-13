093651ca1f6d399f6db4c71e6f973c1d1a11bfa9
Christian Merten <christian@merten.dev>
2025-01-13 04:57:07 +0100

    add height file

--- Polydim/Dimension/Homomorphism.lean
+++ Polydim/Dimension/Homomorphism.lean
@@ -1,29 +1,27 @@
 import Mathlib
+import Polydim.Dimension.Height
 
 variable {A B : Type*} [CommRing A] [CommRing B] [Algebra A B]
 variable [IsNoetherianRing A] [IsNoetherianRing B]
 
--- TODO: replace by better definition
-/-- The height of a prime ideal in a ring is the supremum over the lengths of prime ideal
-chains ending at `p`. -/
-noncomputable def Ideal.primeHeight (p : Ideal A) : WithTop ℕ :=
-  Set.chainHeight { q : Ideal A | q.IsPrime ∧ q < p }
-
-/-- The height of a prime ideal `p` equals to the dimension of `A` localized away from `p`. -/
-lemma Ideal.primeHeight_eq_ringKrullDim (p : Ideal A) [p.IsPrime] :
-    p.primeHeight = ringKrullDim (Localization.AtPrime p) :=
+-- this needs some more inputs on dimension theory first
+lemma ringKrullDim_le_ringKrullDim_add_of_isLocalRing [IsLocalRing A] [IsLocalRing B] :
+    ringKrullDim B ≤ ringKrullDim A +
+      ringKrullDim (B ⧸ (IsLocalRing.maximalIdeal A).map (algebraMap A B)) :=
   sorry
 
 /-- Matsumura 13.B Th. 19 (1) -/
 lemma primeHeight_le_primeHeight_add_of_liesOver (p : Ideal A) [p.IsPrime] (P : Ideal B) [P.IsPrime]
       [P.LiesOver p] :
     P.primeHeight ≤ p.primeHeight +
-      (Ideal.map (algebraMap B <| B ⧸ Ideal.map (algebraMap A B) p) P).primeHeight :=
+      (Ideal.map (algebraMap B <| B ⧸ Ideal.map (algebraMap A B) p) P).primeHeight := by
   sorry
 
 /-- Matsumura 13.B Th. 19 (2) -/
-lemma primeHeight_eq_primeHeight_add_of_liesOver_of_flat (p : Ideal A) [p.IsPrime] (P : Ideal B)
-    [P.IsPrime] [P.LiesOver p] :
+lemma primeHeight_eq_primeHeight_add_of_liesOver_of_flat [Module.Flat A B] (p : Ideal A) [p.IsPrime]
+    (P : Ideal B) [P.IsPrime] [P.LiesOver p] :
     P.primeHeight = p.primeHeight +
       (Ideal.map (algebraMap B <| B ⧸ Ideal.map (algebraMap A B) p) P).primeHeight :=
+  -- use `primeHeight_le_primeHeight_add_of_liesOver` for one direction
+  -- and going down for the other one
   sorry
