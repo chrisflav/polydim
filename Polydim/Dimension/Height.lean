b32ce66989726e8741d61a189ca4a8a4a6ac2dc2
su00000 <ssh@stu.pku.edu.cn>
2025-01-13 17:50:41 +0800

    Update Height.lean

--- Polydim/Dimension/Height.lean
+++ Polydim/Dimension/Height.lean
@@ -5,7 +5,15 @@ variable {A B : Type*} [CommRing A] [CommRing B]
 /-- The height of a prime ideal in a ring is the supremum over the lengths of prime ideal
 chains ending at `p`. -/
 noncomputable def Ideal.primeHeight (p : Ideal A) : WithTop ℕ :=
-  Set.chainHeight { q : Ideal A | q.IsPrime ∧ q < p }
+  Set.chainHeight { q : Ideal A | q.IsPrime ∧ q ≤ p }
+
+lemma chainHeight_eq_orderHeight {α : Type*} [Preorder α] (p : α) :
+    Set.chainHeight { q : α | q ≤ p } = Order.height p := by
+  sorry
+
+lemma Ideal.primeHeight_eq_orderheight (p : Ideal A) [hp : p.IsPrime] :
+    Ideal.primeHeight p = Order.height (⟨p, hp⟩ : PrimeSpectrum A) := by
+  sorry
 
 -- TODO: remove this in favour of `IsLocalization.primeHeight_eq_ringKrullDim`
 /-- The height of a prime ideal `p` equals to the dimension of `A` localized away from `p`. -/
@@ -70,8 +78,42 @@ lemma IsLocalization.primeHeight_eq_ringKrullDim (p : Ideal A) [p.IsPrime]
 /-- `dim A ≤ n` if and only if the height of all prime ideals is less than `n`. -/
 lemma ringKrullDim_le_of_height_le [Nontrivial A] (n : WithBot (WithTop ℕ)) :
   ringKrullDim A ≤ n ↔
-    ∀ (p : Ideal A), p.IsPrime → p.primeHeight ≤ n :=
-  sorry
+    ∀ (p : Ideal A), p.IsPrime → p.primeHeight ≤ n := by
+  constructor
+  · intro h p hp
+    rw [WithBot.coe_eq_coe.mpr (Ideal.primeHeight_eq_orderheight p)]
+    exact Preorder.le_trans _ _ n (Order.height_le_krullDim (⟨p, hp⟩ : PrimeSpectrum A)) h
+  · intro h
+    by_cases ht : n = ⊤
+    · exact ht ▸ OrderTop.le_top (ringKrullDim A)
+    by_cases hb : n = ⊥
+    · obtain ⟨p, hp⟩ := Ideal.exists_maximal A
+      have := h p hp.isPrime
+      simp only [hb, le_bot_iff, WithBot.coe_ne_bot] at this
+    by_contra! hlt
+    by_cases hab : ringKrullDim A = ⊥
+    · simp only [hab, not_lt_bot] at hlt
+    obtain ⟨n1, hn1⟩ := WithBot.ne_bot_iff_exists.mp hb
+    obtain ⟨m1, hm1⟩ := WithBot.ne_bot_iff_exists.mp hab
+    rw [← hn1, WithBot.coe_eq_top] at ht
+    obtain ⟨n2, hn2⟩ := WithTop.ne_top_iff_exists.mp ht
+    rw [← hn1, ← hm1, WithBot.coe_lt_coe] at hlt
+    have : n + 1 ≤ ringKrullDim A := by
+      by_cases hat : ringKrullDim A = ⊤
+      · exact hat ▸ OrderTop.le_top (n + 1)
+      rw [← hm1, WithBot.coe_eq_top] at hat
+      obtain ⟨m2, hm2⟩ := WithTop.ne_top_iff_exists.mp hat
+      rw [← hn2, ← hm2, ENat.some_eq_coe, Nat.cast_lt] at hlt
+      rw [← hm1, ← hn1, ← WithBot.coe_one, ← WithBot.coe_add n1 1, ← hn2, ← hm2, ← WithTop.coe_one, ← WithTop.coe_add n2 1]
+      refine WithBot.coe_le_coe.mpr <| WithTop.coe_le_coe.mpr hlt
+    rw [ringKrullDim, ← hn1, ← WithBot.coe_one, ← WithBot.coe_add n1 1, ← hn2, ← WithTop.coe_one, ← WithTop.coe_add n2 1] at this
+    obtain ⟨l, hl⟩ := Order.le_krullDim_iff.mp this
+    have := h l.last.asIdeal l.last.isPrime
+    rw [Ideal.primeHeight_eq_orderheight, ← hn1, WithBot.coe_le_coe, Order.height_le_iff] at this
+    have := this (p := l) (by rfl)
+    rw [hl, ← hn2, ENat.some_eq_coe, Nat.cast_le] at this
+    have : n2 + 1 ≤ n2 := this
+    simp only [add_le_iff_nonpos_right, nonpos_iff_eq_zero, one_ne_zero] at this
 
 lemma Ideal.exists_isMaximal_height_eq_of_nontrivial [Nontrivial A] :
     ∃ (p : Ideal A), p.IsMaximal ∧ p.primeHeight = ringKrullDim A :=
